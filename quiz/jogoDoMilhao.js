48d71ae921c8f83e10575540f8e7d09793cef9dd
MostraDeLinguagensInfo0123 <mostradelinguagens.info0123@gmail.com>
2024-06-30 13:12:29 -0300

    Add files via upload

--- quiz/jogoDoMilhao.js
+++ quiz/jogoDoMilhao.js
@@ -1,6 +1,3 @@
-function voltarPagina() {
-    window.history.back();
-}
 let introducao = document.getElementById('introducao');
 let respA = document.getElementById('letraA');
 let respB = document.getElementById('letraB');
@@ -15,6 +12,9 @@ let travarApagar = 0;
 let paragrafoApagar = document.getElementById('paragrafoApagar');
 let botaoApagar = document.getElementById('apagar');
 let quantidadePerguntas = 0
+let audioAcerto = document.getElementById('audioAcertoid');
+let audioErro = document.getElementById('audioErroid');
+let audioTermino = document.getElementById('audioTerminoid');
 
 function escolherPergunta() {
     let perguntaAleatoria = Math.ceil(Math.random() * 5);
@@ -47,37 +47,61 @@ function comecarJogo() {
                     pergunta1();
                     quantidadePerguntas++
                     console.log('quantidade: '+quantidadePerguntas)
-
-                  
+                    if(travarApagar===1){
+                        respA.style.display = 'inline';
+                        respB.style.display = 'inline';
+                        respC.style.display = 'inline';
+                        respD.style.display = 'inline';
+                    }
                     break;
                 case 2:
                     pergunta2();
                     quantidadePerguntas++
                     console.log('quantidade: '+quantidadePerguntas)
-
-                    
+                    if(travarApagar===1){
+                        respA.style.display = 'inline';
+                        respB.style.display = 'inline';
+                        respC.style.display = 'inline';
+                        respD.style.display = 'inline';
+                    }
                     break;
                 case 3:
                     pergunta3();
                     quantidadePerguntas++
                     console.log('quantidade: '+quantidadePerguntas)
-
-                    
+                    if(travarApagar===1){
+                        respA.style.display = 'inline';
+                        respB.style.display = 'inline';
+                        respC.style.display = 'inline';
+                        respD.style.display = 'inline';
+                    }
                     break;
                 case 4:
                     pergunta4();
                     quantidadePerguntas++
                     console.log('quantidade: '+quantidadePerguntas)
-
-                    
+                    if(travarApagar===1){
+                        respA.style.display = 'inline';
+                        respB.style.display = 'inline';
+                        respC.style.display = 'inline';
+                        respD.style.display = 'inline';
+                    }
                     break;
                 case 5:
                     pergunta5();
                     quantidadePerguntas++
                     console.log('quantidade: '+quantidadePerguntas)
+                    if(travarApagar===1){
+                        respA.style.display = 'inline';
+                        respB.style.display = 'inline';
+                        respC.style.display = 'inline';
+                        respD.style.display = 'inline';
+                    }
                     break;
             }
         }}
+        
+
 function pergunta1() {
     question.innerHTML = 'Qual o anagrama do nome Iracema?';
     respA.innerHTML = 'Europa';
@@ -86,67 +110,67 @@ function pergunta1() {
     respB.value = 'Brasil';
     respC.innerHTML = 'América';
     respC.value = 'América';
-    respD.innerHTML = 'Índia';
-    respD.value = 'Índia';
+    respD.innerHTML = 'India';
+    respD.value = 'India';
     respCorreta = 'C';
 }
-
 function pergunta2() {
     question.innerHTML = 'Quais são os livros que compõem a trilogia indianista de José de Alencar?';
-    respA.innerHTML = 'O Guarani, Iracema e Ubirajara.';
+    respA.innerHTML = ' O Guarani, Iracema e Ubirajara.';
     respA.value = 'O Guarani, Iracema e Ubirajara.';
-    respB.innerHTML = 'Minas de Prata, O Guarani e Iracema.';
-    respB.value = 'Minas de Prata, O Guarani e Iracema.';
+    respB.innerHTML = 'Minas de Prata, Guarani e Iracema.';
+    respB.value = 'Minas de Prata, Guarani e Iracema.';
     respC.innerHTML = 'Senhora, Diva e Ubirajara.';
     respC.value = 'Senhora, Diva e Ubirajara.';
-    respD.innerHTML = 'Diva, Iracema e Senhora.';
-    respD.value = 'Diva, Iracema e Senhora.';
-    respCorreta = 'A';
+    respD.innerHTML = 'Diva, Iracema e Senhora.';
+    respD.value = 'Diva, Iracema e Senhora.';
+    respCorreta = 'A';  
 }
 
 function pergunta3() {
-    question.innerHTML = 'Qual a diferença entre o uso dos termos "índio" e "indígena"?';
+    question.innerHTML = 'Qual a diferença do uso do termo "índio" para "indígena?';
     respA.innerHTML = 'Os termos são equivalentes e possuem o mesmo significado.';
     respA.value = 'Os termos são equivalentes e possuem o mesmo significado.';
-    respB.innerHTML = 'O termo "índio" é o mais correto para se referir aos povos nativos de um local.';
-    respB.value = 'O termo "índio" é o mais correto para se referir aos povos nativos de um local.';
+    respB.innerHTML = 'O  termo "índio" é o mais correto para se referir aos povos nativos de um local.';
+    respB.value = 'O  termo "índio" é o mais correto para se referir aos povos nativos de um local.';
     respC.innerHTML = '"Indígena" é um termo mais adequado e respeitoso porque significa "originário do lugar".';
     respC.value = '"Indígena" é um termo mais adequado e respeitoso porque significa "originário do lugar".';
-    respD.innerHTML = '"Índio" é um termo mais recente e foi criado para substituir "indígena".';
-    respD.value = '"Índio" é um termo mais recente e foi criado para substituir "indígena".';
+    respD.innerHTML = '"Índio" é um termo mais recente e foi criado para substituir "indígena".';
+    respD.value = '"Índio" é um termo mais recente e foi criado para substituir "indígena".';
     respCorreta = 'C';
 }
-
 function pergunta4() {
     question.innerHTML = 'Atualmente, como é a vida das comunidades indígenas no Brasil?';
-    respA.innerHTML = 'As comunidades indígenas no Brasil vivem em total harmonia, com suas terras respeitadas e protegidas pelo governo.';
+    respA.innerHTML = 'As comunidades indígenas no Brasil vivem em total harmonia, com suas terras respeitadas e protegidas pelo governo.'
     respA.value = 'As comunidades indígenas no Brasil vivem em total harmonia, com suas terras respeitadas e protegidas pelo governo.';
     respB.innerHTML = 'Muitas comunidades indígenas enfrentam invasões de terras, desmatamento e falta de serviços básicos.';
-    respB.value = 'Muitas comunidades indígenas enfrentam invasões de terras, desmatamento e falta de serviços básicos.';
+    respB.value = ' Muitas comunidades indígenas enfrentam invasões de terras, desmatamento e falta de serviços básicos.';
     respC.innerHTML = 'As terras indígenas são frequentemente convertidas em áreas de turismo sustentável, proporcionando renda às comunidades.';
     respC.value = 'As terras indígenas são frequentemente convertidas em áreas de turismo sustentável, proporcionando renda às comunidades.';
-    respD.innerHTML = 'As comunidades indígenas têm pleno acesso a tecnologias modernas e infraestrutura urbana.';
-    respD.value = 'As comunidades indígenas têm pleno acesso a tecnologias modernas e infraestrutura urbana.';
+    respD.innerHTML = 'As comunidades indígenas têm pleno acesso a tecnologias modernas e infraestrutura urbana.';
+    respD.value = 'As comunidades indígenas têm pleno acesso a tecnologias modernas e infraestrutura urbana.';
     respCorreta = 'B';
+    
 }
-
 function pergunta5() {
-    question.innerHTML = 'Quantos qual a porcentagem de indígenas que temos no Brasil atualmente em comparação com a quantidade na época da chegada dos portugueses?';
+    question.innerHTML = 'Quantos por cento de indígenas temos no Brasil atualmente em comparação com a quantidade na época da chegada dos portugueses?';
     respA.innerHTML = 'Atualmente, temos aproximadamente 10% da população indígena que existia no Brasil na época da chegada dos portugueses.';
     respA.value = 'Atualmente, temos aproximadamente 10% da população indígena que existia no Brasil na época da chegada dos portugueses.';
     respB.innerHTML = 'Atualmente, temos aproximadamente 50% da população indígena que existia no Brasil na época da chegada dos portugueses.';
     respB.value = 'Atualmente, temos aproximadamente 50% da população indígena que existia no Brasil na época da chegada dos portugueses.';
-    respC.innerHTML = 'Atualmente, temos aproximadamente 25% da população indígena que existia no Brasil na época da chegada dos portugueses.';
-    respC.value = 'Atualmente, temos aproximadamente 25% da população indígena que existia no Brasil na época da chegada dos portugueses.';
+    respC.innerHTML = 'Atualmente, temos aproximadamente 25% da população indígena que existia no Brasil na época da chegada dos portugueses.';
+    respC.value = 'Atualmente, temos aproximadamente 25% da população indígena que existia no Brasil na época da chegada dos portugueses.';
     respD.innerHTML = 'Atualmente, temos aproximadamente 1% da população indígena que existia no Brasil na época da chegada dos portugueses.';
     respD.value = 'Atualmente, temos aproximadamente 1% da população indígena que existia no Brasil na época da chegada dos portugueses.';
     respCorreta = 'D';
 }
-
 function VerificarRespostaBotaoA() {
     if(quantidadePerguntas<=5){
     if (respCorreta == 'A' && jaComecou == 0) {
         valor += 1;
+        audioAcerto.play();
+    }else{
+        audioErro.play();
     }
     mudarCor();
     setTimeout(corPadrao, 2000);
@@ -157,7 +181,8 @@ function VerificarRespostaBotaoA() {
     setTimeout(corPadrao, 2000);
     setTimeout(verificarPergunta, 2000);
         setTimeout(function() {
-            introducao.innerHTML = `Quiz finalizado ✔`
+            introducao.innerHTML = `Quiz finalizado`
+            audioTermino.play();
     jaComecou=1;
     respA.style.display = 'none';
     respB.style.display = 'none';
@@ -175,6 +200,9 @@ function VerificarRespostaBotaoB() {
     if(quantidadePerguntas<=5){
     if (respCorreta == 'B' && jaComecou == 0) {
         valor += 1;
+        audioAcerto.play()
+    }else{
+        audioErro.play();
     }
     mudarCor();
     setTimeout(corPadrao, 2000);
@@ -185,7 +213,8 @@ function VerificarRespostaBotaoB() {
     setTimeout(corPadrao, 2000);
     setTimeout(verificarPergunta, 2000);
         setTimeout(function() {
-            introducao.innerHTML = `Quiz finalizado ✔`
+            introducao.innerHTML = `Quiz finalizado`;
+            audioTermino.play();
     jaComecou=1;
     respA.style.display = 'none';
     respB.style.display = 'none';
@@ -203,6 +232,9 @@ function VerificarRespostaBotaoC() {
     if(quantidadePerguntas<=5){
     if (respCorreta == 'C' && jaComecou == 0) {
         valor += 1;
+        audioAcerto.play();
+    }else{
+        audioErro.play();
     }
     mudarCor();
     setTimeout(corPadrao, 2000);
@@ -214,7 +246,8 @@ if(quantidadePerguntas===5){
     setTimeout(corPadrao, 2000);
     setTimeout(verificarPergunta, 2000);
         setTimeout(function() {
-            introducao.innerHTML = `Quiz finalizado ✔`
+            introducao.innerHTML = `Quiz finalizado`
+            audioTermino.play();
             jaComecou=1;
             respA.style.display = 'none';
             respB.style.display = 'none';
@@ -231,6 +264,9 @@ function VerificarRespostaBotaoD() {
     if(quantidadePerguntas<=5){
     if (respCorreta == 'D' && jaComecou == 0) {
         valor += 1;
+        audioAcerto.play();
+    }else{
+        audioErro.play();
     }
     mudarCor();
     setTimeout(corPadrao, 2000);
@@ -242,7 +278,8 @@ if(quantidadePerguntas===5){
     setTimeout(corPadrao, 2000);
     setTimeout(verificarPergunta, 2000);
         setTimeout(function() {
-            introducao.innerHTML = `Quiz finalizado ✔`
+            introducao.innerHTML = `Quiz finalizado`
+            audioTermino.play();
     jaComecou=1;
     respA.style.display = 'none';
     respB.style.display = 'none';
@@ -257,28 +294,28 @@ if(quantidadePerguntas===5){
 
 function mudarCor() {
     if (respCorreta == 'A') {
-        respA.style.backgroundColor = '#43ab3a';
-        respB.style.backgroundColor = '#ec3030';
-        respC.style.backgroundColor = '#ec3030';
-        respD.style.backgroundColor = '#ec3030';
+        respA.style.backgroundColor = 'green';
+        respB.style.backgroundColor = 'red';
+        respC.style.backgroundColor = 'red';
+        respD.style.backgroundColor = 'red';
     }
     if (respCorreta == 'B') {
-        respA.style.backgroundColor = '#ec3030';
-        respB.style.backgroundColor = '#43ab3a';
-        respC.style.backgroundColor = '#ec3030';
-        respD.style.backgroundColor = '#ec3030';
+        respA.style.backgroundColor = 'red';
+        respB.style.backgroundColor = 'green';
+        respC.style.backgroundColor = 'red';
+        respD.style.backgroundColor = 'red';
     }
     if (respCorreta == 'C') {
-        respA.style.backgroundColor = '#ec3030';
-        respB.style.backgroundColor = '#ec3030';
-        respC.style.backgroundColor = '#43ab3a';
-        respD.style.backgroundColor = '#ec3030';
+        respA.style.backgroundColor = 'red';
+        respB.style.backgroundColor = 'red';
+        respC.style.backgroundColor = 'green';
+        respD.style.backgroundColor = 'red';
     }
     if (respCorreta == 'D') {
-        respA.style.backgroundColor = '#ec3030';
-        respB.style.backgroundColor = '#ec3030';
-        respC.style.backgroundColor = '#ec3030';
-        respD.style.backgroundColor = '#43ab3a';
+        respA.style.backgroundColor = 'red';
+        respB.style.backgroundColor = 'red';
+        respC.style.backgroundColor = 'red';
+        respD.style.backgroundColor = 'green';
     }
 }
 function corPadrao() {
@@ -351,10 +388,15 @@ function apagarEscolhas() {
                     break;
             }
         }
-        travarApagar++;
+        travarApagar=1;
     }
-    
 }
+    if(travarApagar===1){
+        respA.style.display = 'inline';
+        respB.style.display = 'inline';
+        respC.style.display = 'inline';
+        respD.style.display = 'inline';
+    }
    
 botaoApagar.addEventListener('click', apagarEscolhas);
 console.log(travarApagar);
