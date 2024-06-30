09a53f1c1942046d734edb2af4cddca7f3fc1021
MostraDeLinguagensInfo0123 <mostradelinguagens.info0123@gmail.com>
2024-06-29 22:08:35 -0300

    Add files via upload

---
+++ biblioteca/script/script.js
@@ -0,0 +1,97 @@
+function voltarPagina() {
+    window.history.back();
+}
+const popup = document.getElementById('popup-card');
+const overlay = document.getElementById('popup');
+const text = popup.querySelector('.resumo');
+const img = popup.querySelector('.imagePopup');
+
+function ubirajara() {
+
+    img.src = 'img/ubirajara.jpg'
+    text.innerHTML = `
+    "Ubirajara", publicado em 1874 por José de Alencar, faz parte da trilogia indianista do autor, que inclui também "O Guarani" e "Iracema". O romance é ambientado no Brasil pré-colonial e narra a história de Jaguarê, um jovem guerreiro da tribo Araguaia, que se torna conhecido como Ubirajara, o "Senhor da Lança", após derrotar Pojucã, um guerreiro da tribo Tocantins. A narrativa celebra valores indígenas como honra, coragem e respeito à natureza, e Alencar utiliza uma linguagem rica e poética para descrever a flora e fauna brasileiras. No entanto, a idealização presente na obra pode ser vista como uma romantização que não reflete com precisão a complexidade e a realidade histórica dos povos indígenas.<br><br>
+
+    A trama também aborda o romance entre Ubirajara e Araci, filha do chefe Itaquê da tribo Tocantins. O relacionamento deles é marcado por desafios que Ubirajara deve superar para conquistar seu amor e a aceitação da tribo de Araci. Embora o resumo destaque esse aspecto, ele poderia se beneficiar de uma maior profundidade na descrição da evolução emocional dos personagens e das provas enfrentadas. <br><br>
+
+    José de Alencar busca construir uma identidade nacional brasileira exaltando a cultura indígena, enaltecendo heróis e tradições. No entanto, essa representação pode ser criticada por ser simplista e idealista, ignorando os aspectos negativos e os conflitos internos das sociedades indígenas. A linguagem poética e as descrições vívidas da natureza ajudam a criar um ambiente imersivo, mas o foco excessivo na estética pode desviar a atenção da narrativa central e do desenvolvimento dos personagens. <br><br>
+
+    Em resumo, "Ubirajara" é uma obra que celebra a força e a beleza da cultura indígena brasileira, oferecendo uma visão romântica dos tempos pré-coloniais. Apesar de Alencar tentar promover uma identidade nacional baseada nas raízes indígenas, a narrativa pode ser criticada pela sua idealização e simplificação da realidade indígena. A linguagem poética e as descrições vívidas são pontos fortes da obra, mas a falta de profundidade em alguns aspectos enfraquece a construção dos personagens e a complexidade da trama. <br><br>`
+    popup.style.display = "block";
+    overlay.style.display = "block";
+    
+
+}
+
+function iracema() {
+
+    img.src = 'img/iracema.jpeg'
+    text.innerHTML = `"Iracema", publicado em 1865 por José de Alencar, é um dos romances mais conhecidos do autor e faz parte da trilogia indianista que inclui também "O Guarani" e "Ubirajara". A história se passa no Brasil colonial, durante o período da colonização portuguesa.<br><br>
+
+    O enredo gira em torno de Iracema, a "virgem dos lábios de mel", uma bela índia tabajara que vive na floresta brasileira. Ela se apaixona por Martim, um guerreiro português, e os dois vivem um romance proibido que simboliza o encontro entre as culturas indígena e europeia. A narrativa é marcada por uma linguagem poética e descritiva, que exalta a natureza exuberante do Brasil e os costumes dos povos indígenas.<br><br>
+    
+    José de Alencar utiliza o romance entre Iracema e Martim para explorar temas como amor, identidade cultural, e os conflitos entre os povos nativos e os colonizadores europeus. A obra é também uma crítica à colonização e à exploração dos recursos naturais do Brasil.<br><br>
+    
+    Em "Iracema", Alencar celebra a beleza e a cultura dos povos indígenas brasileiros, oferecendo uma visão romântica e idealizada dos tempos coloniais. No entanto, a simplificação e idealização da realidade indígena podem ser criticadas, assim como a representação dos personagens e dos conflitos. Alencar retrata os indígenas de forma idealizada e exótica, sem explorar totalmente a complexidade e os desafios enfrentados por essas comunidades durante o período colonial. A linguagem poética e as descrições vívidas da natureza são pontos fortes da obra, contribuindo para criar um ambiente imersivo e emocionante, mas alguns críticos apontam que a romantização pode obscurecer uma análise mais profunda das dinâmicas sociais e históricas envolvidas.<br><br>
+    
+    Em resumo, "Iracema" é uma leitura essencial para quem deseja compreender a visão romântica do período colonial brasileiro, mas também pode ser analisada criticamente por sua representação estereotipada e simplificada dos povos indígenas e da colonização.<br><br>`
+    
+    popup.style.display = "block";
+    overlay.style.display = "block";
+    
+
+}
+
+function guarani() {
+
+    img.src = 'img/guarani.jpg'
+    text.innerHTML = `"O Guarani", publicado em 1857 por José de Alencar, é um dos romances mais conhecidos do autor e faz parte da trilogia indianista que inclui também "Iracema" e "Ubirajara". A história se passa no Brasil colonial, durante o século XVI, e narra os conflitos entre colonizadores portugueses e a resistência dos povos indígenas.<br><br>
+
+    O protagonista, Peri, é um jovem guerreiro guarani, conhecido por sua bravura e habilidades na selva. Ele se apaixona por Cecília, filha de um fidalgo português, mas enfrenta a oposição do pai dela, Dom Antônio de Mariz. A narrativa explora temas como amor, honra, vingança e a relação entre as culturas indígena e europeia.<br><br>
+    
+    José de Alencar utiliza uma linguagem poética e descritiva para descrever a exuberante natureza brasileira, bem como os costumes e crenças dos povos indígenas. A obra também aborda críticas à colonização e a exploração dos recursos naturais.<br><br>
+    
+    Em "O Guarani", Alencar celebra a cultura e a resistência dos povos indígenas brasileiros, oferecendo uma visão romântica e idealizada dos tempos coloniais. No entanto, a simplificação e idealização da realidade indígena podem ser criticadas, assim como a representação dos personagens e dos conflitos. Alencar retrata os indígenas de forma idealizada e heroica, sem explorar completamente a complexidade das relações entre as diferentes tribos e entre indígenas e colonizadores. A linguagem poética e as descrições vívidas da natureza são pontos fortes da obra, contribuindo para criar um ambiente imersivo e emocionante. No entanto, alguns críticos apontam que essa romantização pode obscurecer uma análise mais profunda das dinâmicas sociais e históricas envolvidas, limitando a compreensão das reais dificuldades enfrentadas pelos povos indígenas durante a colonização.<br><br>
+    
+    Em resumo, "O Guarani" é uma leitura fundamental para quem deseja entender a visão romântica do período colonial brasileiro, mas também pode ser analisada criticamente por sua representação estereotipada e simplificada dos povos indígenas e da colonização.
+    <br><br>`
+    popup.style.display = "block";
+    overlay.style.display = "block";
+}
+
+function util() {
+
+    img.src = 'img/A Vida Não é Útil.jpeg'
+    text.innerHTML = `No livro "A Vida Não É Útil," Ailton Krenak oferece uma profunda reflexão sobre as crises contemporâneas exacerbadas pela pandemia de COVID-19. <br> Ele critica as tendências destrutivas da sociedade moderna, como o consumismo desenfreado, a devastação ambiental e uma visão limitada e excludente da humanidade. <br> Krenak argumenta que a busca incessante por progresso e desenvolvimento material tem levado à destruição do planeta e ao esgotamento dos recursos naturais. <br><br> O autor também destaca a importância de repensar nossos valores e modos de vida, propondo uma reconexão com a natureza e a adoção de formas de existência mais sustentáveis e inclusivas. <br> Inspirado nas tradições e sabedorias indígenas, Krenak sugere que podemos aprender muito com as culturas ancestrais sobre como viver em harmonia com o meio ambiente e com os outros seres humanos. <br><br> Além disso, Krenak questiona a noção de que a vida deve ser útil no sentido capitalista e produtivista. <br> Ele convida os leitores a refletirem sobre a necessidade de desacelerar e valorizar a simplicidade, a contemplação e a convivência respeitosa com todas as formas de vida. <br> O livro é um convite à reflexão profunda sobre o rumo da civilização e a urgência de mudanças para garantir a sobrevivência do planeta e das futuras gerações.`
+    popup.style.display = "block";
+    overlay.style.display = "block";
+}
+
+function nos() {
+
+    img.src = 'img/nós.jpeg'
+    text.innerHTML = `Nesta belíssima antologia ilustrada, o leitor vai conhecer dez histórias contadas ou recontadas por escritores de diferentes nações indígenas. <br><br> "Nós: Uma antologia de literatura indígena" oferece uma rica coleção de narrativas que celebram a diversidade cultural e a sabedoria ancestral dos povos indígenas. <br> Cada história traz uma perspectiva única, refletindo as tradições, os mitos e as experiências vividas por essas comunidades. <br><br> Além de proporcionar entretenimento, o livro serve como uma ponte para o entendimento e a valorização da cultura indígena, convidando o leitor a mergulhar em um universo repleto de simbolismo e ensinamentos profundos. <br> A antologia destaca a importância de preservar essas histórias e reconhece o papel vital da literatura na manutenção da identidade e do patrimônio cultural dos povos indígenas. <br><br> A antologia foi escrita por diversos autores indígenas, cada um trazendo sua perspectiva única e rica em tradição cultural. <br> Esses escritores representam diferentes nações indígenas, contribuindo com suas histórias e recontos para criar uma coletânea diversificada e significativa. <br> O livro reflete a colaboração e o esforço coletivo desses autores em preservar e compartilhar suas narrativas e sabedorias ancestrais.`
+    popup.style.display = "block";
+    overlay.style.display = "block";
+}
+
+function ceu() {
+
+    img.src = 'img/ceu.jpeg'
+    text.innerHTML = `"A Queda do Céu" é uma esplêndida sessão xamânica guiada pelo líder yanomami Davi Kopenawa. <br><br> O livro aborda elementos da cultura yanomami, oferecendo uma profunda visão de mundo baseada nas tradições e crenças dessa comunidade indígena. <br> Kopenawa explora a importância das práticas xamânicas para a saúde do universo, destacando como essas práticas são fundamentais para manter o equilíbrio e a harmonia entre os seres humanos, a natureza e os espíritos. <br><br> Além disso, a obra é um testemunho poderoso que vem da floresta, trazendo a legitimidade e a sabedoria dos espíritos yanomami. <br> Kopenawa compartilha suas experiências e conhecimentos com uma autenticidade que convida os leitores a entenderem e respeitarem a rica herança cultural dos yanomami. <br><br> "A Queda do Céu" não é apenas um livro, mas uma jornada espiritual que oferece uma visão única e valiosa sobre a conexão profunda entre os yanomami e o universo que os rodeia.`
+    popup.style.display = "block";
+    overlay.style.display = "block";
+}
+
+function indigenas() {
+
+    img.src = 'img/indígenas.jpg'
+    text.innerHTML = `Este é um livro cheio de curiosidades que oferece ao leitor um passeio pelos costumes de quatro povos indígenas diferentes: os Yanomami, os Krahô, os Kuikuro e os Guarani Mbya. <br><br> 'Aldeias, palavras e mundos indígenas' é uma obra rica e envolvente que apresenta a diversidade cultural desses povos. <br> O livro mergulha nos modos de vida, tradições, crenças e práticas cotidianas das comunidades, proporcionando uma compreensão mais profunda e respeitosa de suas culturas. <br><br> Através de uma narrativa acessível e informativa, os leitores são convidados a explorar as aldeias e os mundos indígenas, descobrindo a beleza e a complexidade das formas de vida que muitas vezes são desconhecidas ou mal compreendidas. <br> 'Aldeias, palavras e mundos indígenas' não apenas educa, mas também celebra a riqueza cultural dos Yanomami, Krahô, Kuikuro e Guarani Mbya, destacando a importância de preservar e valorizar essas tradições ancestrais para as futuras gerações.`
+    popup.style.display = "block";
+    overlay.style.display = "block";
+}
+
+function closePopup() {
+    popup.style.display = "none";
+    overlay.style.display = "none";
+}
